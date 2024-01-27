2595f9fc30b32211ed75f9bdd6d4ae317f6e74b4
Mateus Deitos <mate.deitos@hotmail.com>
2024-01-27 13:30:18 -0300

    elixir: name_badege

--- elixir/name-badge/lib/name_badge.ex
+++ elixir/name-badge/lib/name_badge.ex
@@ -1,5 +1,22 @@
 defmodule NameBadge do
-  def print(id, name, department) do
-    # Please implement the print/3 function
+  def print(id, name, department)
+      when id !== nil and department !== nil do
+    "[#{id}] - #{name} - #{String.upcase(department)}"
+  end
+
+  # new employee
+  def print(nil, name, department) when department !== nil do
+    "#{name} - #{String.upcase(department)}"
+  end
+
+  def print(id, name, nil), do: print_owner(id, name)
+  def print(nil, name, nil), do: print_owner(nil, name)
+
+  defp print_owner(id, name) do
+    if id === nil do
+      "#{name} - OWNER"
+    else
+      "[#{id}] - #{name} - OWNER"
+    end
   end
 end
